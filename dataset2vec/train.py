Add lr scheduler; Change standarization to scaling; Minor UX fixes; Add training script

dataset2vec/train.py
@@ -4,6 +4,7 @@ from typing import Any, Mapping
 import pytorch_lightning as pl
 import torch
 from torch import Tensor, optim
+from torch.optim.lr_scheduler import LinearLR
 
 from dataset2vec.config import OptimizerConfig
 
@@ -149,9 +150,21 @@ class LightningBase(pl.LightningModule, ABC):
             labels.append(label)
         return torch.Tensor(labels), torch.stack(similarities)
 
-    def configure_optimizers(self) -> optim.Optimizer:
-        return self.optimizer_cls(  # type: ignore
+    def configure_optimizers(
+        self,
+    ) -> tuple[list[optim.Optimizer], list[dict[str, Any]]]:
+        optimizer = self.optimizer_cls(  # type: ignore
             self.parameters(),
             lr=self.learning_rate,
             weight_decay=self.weight_decay,
         )
+        scheduler = LinearLR(optimizer)
+
+        return [optimizer], [
+            {
+                "scheduler": scheduler,
+                "interval": "epoch",
+                "monitor": "val_accuracy",
+                "frequency": 1,
+            }
+        ]

test/dataset2vec/test_utils.py
@@ -19,8 +19,8 @@ def test_preprocessing_pipeline() -> None:
         {
             "pipeline-1__col3_A": [1, 0, 1],
             "pipeline-1__col3_B": [0, 1, 0],
-            "pipeline-2__col1": [-1 * np.sqrt(1.5), 0, 1 * np.sqrt(1.5)],
-            "pipeline-2__col2": [-1 * np.sqrt(1.5), 0, 1 * np.sqrt(1.5)],
+            "pipeline-2__col1": [0, 0.5, 1],
+            "pipeline-2__col2": [0, 0.5, 1],
         }
     )
 
@@ -91,6 +91,23 @@ def test_is_positive_when_improper_input() -> None:
     # Then
     with pytest.raises(AssertionError):
         Validators.is_positive(input)
+
+
+def test_non_negative_when_proper_input() -> None:
+    # Given
+    input = 0
+
+    # Then
+    Validators.non_negative(input)
+
+
+def test_non_negative_when_improper_input() -> None:
+    # Given
+    input = -11
+
+    # Then
+    with pytest.raises(AssertionError):
+        Validators.non_negative(input)
 
 
 def test_all_elements_positive_when_proper_input() -> None: